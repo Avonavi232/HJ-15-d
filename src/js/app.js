91e8b288abb490760619a39ad875426c62287860
Avonavi <Avonavi232@gmail.com>
2018-01-11 00:39:56 +0300

    * функционал модального окна доработан * стилизована модалка загрузки картинки * стилизована модалка просмотра картинки (доработать) * подключен icomoon с нужными иконками

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,62 @@
 'use strict';
 
+function wrt(d) {
+    console.log(d);
+}
+
 class Modal{
     constructor(modal){
         this.modal = modal;
+        this.docBody = document.querySelector('body');
+    }
+    closeAnimEnd(){
+        if (this.modal.classList.contains('fadeIn')){
+            this.modal.classList.remove('fadeIn');
+            this.docBody.classList.add('modal-opened');
+        } else if (this.modal.classList.contains('fadeOut')){
+            this.modal.style.display = 'none';
+            this.modal.classList.remove('fadeOut');
+            this.docBody.classList.remove('modal-opened');
+        }
     }
     open(){
-        this.modal.style.display = 'block';
+        this.modal.style.display = 'flex';
+        this.modal.classList.remove('fadeOut');
+        this.modal.classList.add('fadeIn');
     }
     close(){
-        this.modal.style.display = 'none';
+        this.modal.classList.remove('fadeIn');
+        this.modal.classList.add('fadeOut');
     }
     init(){
-        this.modal.querySelector('.js-modal-close').addEventListener('click', e => {
-           this.close();
-        });
+        this.modal.querySelector('.js-modal-close').addEventListener('click', () => {this.close();});
+        this.modal.addEventListener("webkitAnimationEnd", () => {this.closeAnimEnd();});
+        this.modal.addEventListener("animationend", () => {this.closeAnimEnd();});
+        window.addEventListener('click', e => {
+            if (e.target == this.modal) {
+                this.close();
+            }
+        })
     }
 }
 
-const modal = new Modal(document.querySelector('.js-modal'));
-modal.init();
+const showPicModal = new Modal(document.querySelector('.js-show-pic-modal'));
+const uploadModal = new Modal(document.querySelector('.js-upload-modal'));
+showPicModal.init();
+uploadModal.init();
 
-for (const thumbnail of document.querySelectorAll('.card-thumbnail')){
-    thumbnail.addEventListener('click', () => {
-        modal.open();
-    });
-}
+
+document.querySelector('.js-cards-container').addEventListener('click', (e) => {
+    for (const curr of e.path){
+        if(curr.classList.contains('card-thumbnail')){
+            showPicModal.open();
+            break;
+        } else if(curr.tagName === 'BODY'){
+            break; //не поймали, выходим из цикла
+        }
+    }
+});
+document.querySelector('.js-upload-pic').addEventListener('click', (e) => {
+    uploadModal.open();
+});
+
