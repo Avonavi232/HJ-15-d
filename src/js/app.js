5cf6fc8090043333ad8295db288ee2a2831804d2
Avonavi <Avonavi232@gmail.com>
2018-01-11 22:16:08 +0300

    * доработана модалка просмотра картинки * добавлен сайдбар-слайдер с переключением в творческий режим

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ function wrt(d) {
     console.log(d);
 }
 
+//Функционал модалки
 class Modal{
     constructor(modal){
         this.modal = modal;
@@ -33,19 +34,44 @@ class Modal{
         this.modal.addEventListener("webkitAnimationEnd", () => {this.closeAnimEnd();});
         this.modal.addEventListener("animationend", () => {this.closeAnimEnd();});
         window.addEventListener('click', e => {
-            if (e.target == this.modal) {
+            if (e.target === this.modal) {
                 this.close();
             }
         })
     }
 }
 
+//Управляет переключением сайдбара при просмотре иконки
+class ImageSidebar{
+    constructor(classes){
+        this.sidebar = document.querySelector('.' + classes.sidebar);
+        this.art = document.querySelector('.' + classes.art);
+        this.descr = document.querySelector('.' + classes.descr);
+        this.state = true;
+    }
+
+    toggle(){
+        if (this.state){
+            this.descr.style.transform = 'translateX(600px)';
+            this.art.style.transform = 'translateX(0px)';
+            this.state = false;
+        } else {
+            this.descr.style.transform = 'translateX(0px)';
+            this.art.style.transform = 'translateX(-600px)';
+            this.state = true;
+        }
+    }
+}
+
+
+//Инициализация модальных окон
 const showPicModal = new Modal(document.querySelector('.js-show-pic-modal'));
 const uploadModal = new Modal(document.querySelector('.js-upload-modal'));
 showPicModal.init();
 uploadModal.init();
 
 
+//Ловим клик по миниатюре, используя делегирование событий
 document.querySelector('.js-cards-container').addEventListener('click', (e) => {
     for (const curr of e.path){
         if(curr.classList.contains('card-thumbnail')){
@@ -56,7 +82,26 @@ document.querySelector('.js-cards-container').addEventListener('click', (e) => {
         }
     }
 });
+
+
+//Открываем модалку-загрузчик по клику на соотв.кнопке
 document.querySelector('.js-upload-pic').addEventListener('click', (e) => {
     uploadModal.open();
 });
+
+
+//Инициализируем поведение сайдбара при просмотре картинки
+const imageSidebar = new ImageSidebar({
+    sidebar: 'image-sidebar',
+    art: 'image-art',
+    descr: 'image-infoblock'
+});
+
+for (const btn of document.querySelectorAll('.js-image-sidebar-toggle')){
+    btn.addEventListener('click', ()=>{
+        imageSidebar.toggle();
+    })
+}
+
+
 
